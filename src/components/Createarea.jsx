3e4375c35971e72b86fce7b52cc57f633446470b
Priyanshu <priyanshukamboj1903@gmail.com>
2025-11-22 21:34:52 +0530

    Initial updates

--- src/components/Createarea.jsx
+++ src/components/Createarea.jsx
@@ -1,7 +1,14 @@
 import React,{useState} from "react";
-
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 function CreateArea(props){
+    const [isExpandable,setExpandable]=useState(false);
+
     const [note,setNote]=useState({title:"",content:""});
+    function handle(){
+        setExpandable(true);
+    }
     function handleChange(event){
         const {name,value}=event.target;
         setNote(prev=>{return {
@@ -18,9 +25,9 @@ function CreateArea(props){
         event.preventDefault();
     }
     return (<div><form className="create-note">
-        <input type="text" value={note.title} onChange={handleChange} name="title" placeholder="Title"/>
-        <textarea type="text" row="3" placeholder="Take a nore here" value={note.content} onChange={handleChange} name="content"/>
-        <button onClick={SubmitNote}>Add</button>
+        {isExpandable&&<input type="text" value={note.title} onChange={handleChange} name="title" placeholder="Title"/>}
+        <textarea type="text" row={isExpandable?"3":"1"} placeholder="Take a nore here" value={note.content} onChange={handleChange} name="content" onClick={handle}/>
+       <Zoom in={isExpandable}><div> <Fab onClick={SubmitNote}><AddIcon /></Fab></div></Zoom>
         </form></div>);
 }
 export default CreateArea;
